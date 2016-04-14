9e210afdeb65d0bd38893a6f8122604d8c7e128a
valeroclemente <maria.valero59@gmail.com>
2016-04-14 12:35:30 -0400

    Update FiltSerial.c

--- Serial_codes/FiltSerial.c
+++ Serial_codes/FiltSerial.c
@@ -2,7 +2,7 @@
 /************************** SERIAL CODE IMPLEMENTATION ******************************************/
 /*********************** Image Averaging Filter Benchmark *******************************/
 //FILES NEEDED: bmp file of 8 bits (The repository has a sample file)
-//Find files in: Sample Data/lady1000.bmp
+//Find files in: Sample Data/lady1000.bmp//
 
 
 #include <stdio.h>
